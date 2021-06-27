51c4a584c00252548791d7bb9e301bb1eacae287
Davorin <davorin.jurkovic@gmail.com>
2021-06-27 17:10:44 +0200

    Starting at the Buttom with Your DataAccess Layer

---
+++ Books/Books.API/Services/BooksRepository.cs
@@ -0,0 +1,48 @@
+﻿using Books.API.Contexts;
+using Books.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.API.Services
+{
+    public class BooksRepository : IBookRepository, IDisposable
+    {
+        private BookContext context;
+
+        public BooksRepository(BookContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Book> GetBookAsync(Guid id)
+        {
+            return await context.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksAsync()
+        {
+            return await context.Books.Include(b => b.Author).ToListAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                if(context != null)
+                {
+                    context.Dispose();
+                    context = null;
+                }
+            }
+        }
+    }
+}
---
+++ Books/Books.API/Services/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using Books.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.API.Services
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetBooksAsync();
+
+        Task<Book> GetBookAsync(Guid id);
+    }
+}
