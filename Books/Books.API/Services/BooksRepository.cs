86de3206d098e49b4b0e185833c62deade21bc28
Davorin <davorin.jurkovic@gmail.com>
2021-07-05 22:51:42 +0200

    Avoiding Common Pitfalls

--- Books/Books.API/Services/BooksRepository.cs
+++ Books/Books.API/Services/BooksRepository.cs
@@ -2,11 +2,13 @@
 using Books.API.Entities;
 using Books.API.ExternalModels;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Books.API.Services
@@ -15,11 +17,14 @@ namespace Books.API.Services
     {
         private BookContext context;
         private readonly IHttpClientFactory httpClientFactory;
+        private readonly ILogger<BooksRepository> logger;
+        private CancellationTokenSource cancellationTokenSource;
 
-        public BooksRepository(BookContext context, IHttpClientFactory httpClientFactory)
+        public BooksRepository(BookContext context, IHttpClientFactory httpClientFactory, ILogger<BooksRepository> logger)
         {
             this.context = context ?? throw new ArgumentNullException(nameof(context));
             this.httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task<Book> GetBookAsync(Guid id)
@@ -34,8 +39,29 @@ namespace Books.API.Services
             return context.Books.Include(b => b.Author).ToList();
         }
 
+        private Task<int> GetBookPages()
+        {
+            return Task.Run(() =>
+            {
+                var pageCalculator = new Books.Legacy.ComplicatedPageCalculator();
+
+                logger.LogInformation($"ThreadId when calculating the amount of pages: " +
+                    $"{System.Threading.Thread.CurrentThread.ManagedThreadId}");
+
+                return pageCalculator.CalculateBookPages();
+            });
+        }
+
         public async Task<IEnumerable<Book>> GetBooksAsync()
         {
+            //var pageCalculator = new Books.Legacy.ComplicatedPageCalculator();
+            //var amountOfPages = pageCalculator.CalculateBookPages();
+
+            logger.LogInformation($"ThreadId when entering GetBookAsync: " +
+                    $"{System.Threading.Thread.CurrentThread.ManagedThreadId}");
+
+            var bookPages = await GetBookPages();
+
             return await context.Books.Include(b => b.Author).ToListAsync();
         }
 
@@ -61,37 +87,83 @@ namespace Books.API.Services
             return null;
         }
 
+        private async Task<BookCover> DownloadBookCoverAsync(
+            HttpClient httpClient, string bookCoverUrl, CancellationToken cancellationToken)
+        {
+            var response = await httpClient.GetAsync(bookCoverUrl, cancellationToken);
+
+            if(response.IsSuccessStatusCode)
+            {
+                var bookCover = JsonSerializer.Deserialize<BookCover>(
+                    await response.Content.ReadAsStringAsync(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+                return bookCover;
+            }
+
+            cancellationTokenSource.Cancel();
+            return null;
+        }
+
         public async Task<IEnumerable<BookCover>> GetBookCoversAsync(Guid bookId)
         {
             var httpClient = httpClientFactory.CreateClient();
             var bookCovers = new List<BookCover>();
+            cancellationTokenSource = new CancellationTokenSource();
 
             // create a list of fake bookcovers
             var bookCoverUrls = new[]
             {
-                $"https://localhost:44339/api/bookcovers/{bookId}-dummycover1",
-                $"https://localhost:44339/api/bookcovers/{bookId}-dummycover2",
-                $"https://localhost:44339/api/bookcovers/{bookId}-dummycover3",
-                $"https://localhost:44339/api/bookcovers/{bookId}-dummycover4",
-                $"https://localhost:44339/api/bookcovers/{bookId}-dummycover5"
+                $"https://localhost:5001/api/bookcovers/{bookId}-dummycover1",
+                //$"https://localhost:5001/api/bookcovers/{bookId}-dummycover2?returnFault=true",
+                $"https://localhost:5001/api/bookcovers/{bookId}-dummycover2",
+                $"https://localhost:5001/api/bookcovers/{bookId}-dummycover3",
+                $"https://localhost:5001/api/bookcovers/{bookId}-dummycover4",
+                $"https://localhost:5001/api/bookcovers/{bookId}-dummycover5"
             };
 
-            foreach(var bookCoverUrl in bookCoverUrls)
-            {
-                var response = await httpClient.GetAsync(bookCoverUrl);
+            // create the tasks
+            var downloadBookCoverTasksQuery = from bookCoverUrl in bookCoverUrls select DownloadBookCoverAsync(httpClient, bookCoverUrl,
+                cancellationTokenSource.Token);
 
-                if(response.IsSuccessStatusCode)
+            // start the tasks 
+            var downloadBookCoverTasks = downloadBookCoverTasksQuery.ToList();
+
+            try
+            {
+                return await Task.WhenAll(downloadBookCoverTasks);
+            }
+            catch(OperationCanceledException ex)
+            {
+                logger.LogInformation($"{ex.Message}");
+                foreach(var task in downloadBookCoverTasks)
                 {
-                    bookCovers.Add(JsonSerializer.Deserialize<BookCover>(
-                        await response.Content.ReadAsStringAsync(),
-                        new JsonSerializerOptions
-                        {
-                            PropertyNameCaseInsensitive = true,
-                        }));
+                    logger.LogInformation($"Task {task.Id} has status {task.Status}");
                 }
+
+                return new List<BookCover>();
             }
 
-            return bookCovers;
+            
+
+            //foreach(var bookCoverUrl in bookCoverUrls)
+            //{
+            //    var response = await httpClient.GetAsync(bookCoverUrl);
+
+            //    if(response.IsSuccessStatusCode)
+            //    {
+            //        bookCovers.Add(JsonSerializer.Deserialize<BookCover>(
+            //            await response.Content.ReadAsStringAsync(),
+            //            new JsonSerializerOptions
+            //            {
+            //                PropertyNameCaseInsensitive = true,
+            //            }));
+            //    }
+            //}
+
+            //return bookCovers;
         }
 
         public void AddBook(Book bookToAdd)
@@ -124,6 +196,12 @@ namespace Books.API.Services
                 {
                     context.Dispose();
                     context = null;
+                }
+
+                if(cancellationTokenSource != null)
+                {
+                    cancellationTokenSource.Dispose();
+                    cancellationTokenSource = null;
                 }
             }
         }
